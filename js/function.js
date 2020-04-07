243a9085cc283e580f001b2bf2fc7317de5d520c
ec-digit-rsp-mvp <62853393+ec-digit-rsp-mvp@users.noreply.github.com>
2020-04-07 12:05:37 +0200

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -98,3 +98,29 @@ $(document).on('click', '#moveUp', function(){
 $(document).on('click', '#moveDown', function(){
     fullpage_api.moveSectionDown();
 });
+
+// SWIPER SLIDER
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 500,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        clickable: true,
+    },
+})
+
